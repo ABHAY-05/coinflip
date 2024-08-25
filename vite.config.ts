96c697f190caba51c08f3d55b02c7ca9c58b28b4
ABHAY-05 <abhayvermajune5@gmail.com>
2024-08-25 15:00:42 +0530

    changes

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,16 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import react from "@vitejs/plugin-react-swc";
+import { Buffer } from "buffer";
+import { defineConfig } from "vite";
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  resolve: {
+    alias: {
+      buffer: "buffer",
+    },
+  },
+  define: {
+    global: "globalThis",
+    Buffer: Buffer,
+  },
+});
